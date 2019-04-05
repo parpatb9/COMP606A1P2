e4a09af7a0768d604ad2a264bc998874254c2eda
parpatb9 <patelparth1960@gmail.com>
2019-04-06 06:03:59 +1300

    initial commit-4

---
+++ viewAppointment.php
@@ -0,0 +1,42 @@
+<?php include('header.php'); ?>
+
+<div class="jumbotron">
+		
+		<h1 align="center">View Appointments</h1>
+	</div>
+
+
+<div class="container" style="margin: 100px 0px;">
+                               
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>No.</th>
+        <th>All dates</th>
+        <th>Time</th>
+        <th>Status</th>
+        <th>Details</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>11/3/29</td>
+        <td>10:30 am - 11:00 am</td>
+        <td>confirm</td>
+        <td><a href="#">Details</a></td>
+      </tr>
+      <tr>
+         <td>2</td>
+        <td>12/3/29</td>
+        <td>10:30 am - 11:00 am</td>
+        <td>confirm</td>
+        <td><a href="#">Details</a></td>
+      </tr>
+     
+    </tbody>
+  </table>
+</div>
+
+
+<?php include('footer.php'); ?>
